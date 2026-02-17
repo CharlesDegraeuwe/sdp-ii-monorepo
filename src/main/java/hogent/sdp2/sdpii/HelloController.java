be29315cfd397c1a0abc98605275a17b7507e874
CharlesDegraeuwe <charles.degraeuwe@student.hogent.be>
2026-02-17 01:12:30 +0100

    initial commit

---
+++ src/main/java/hogent/sdp2/sdpii/HelloController.java
@@ -0,0 +1,14 @@
+package hogent.sdp2.sdpii;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+public class HelloController {
+    @FXML
+    private Label welcomeText;
+
+    @FXML
+    protected void onHelloButtonClick() {
+        welcomeText.setText("Welcome to JavaFX Application!");
+    }
+}
---
+++ src/main/java/hogent/sdp2/sdpii/Launcher.java
@@ -0,0 +1,9 @@
+package hogent.sdp2.sdpii;
+
+import javafx.application.Application;
+
+public class Launcher {
+    public static void main(String[] args) {
+        Application.launch(HelloApplication.class, args);
+    }
+}
