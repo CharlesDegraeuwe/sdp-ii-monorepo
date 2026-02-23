22e2f942945c74f08e5ac81dca9d3f96d6b3c6a7
CharlesDegraeuwe <charles.degraeuwe@student.hogent.be>
2026-02-23 23:06:41 +0100

    update gebruikersbeheer

--- src/main/java/hogent/sdp2/sdpii/gui/components/app/header/HeaderController.java
+++ src/main/java/hogent/sdp2/sdpii/gui/components/app/header/HeaderController.java
@@ -1,5 +1,7 @@
 package hogent.sdp2.sdpii.gui.components.app.header;
 
+import domain.Sessie;
+import domain.Werknemer;
 import hogent.sdp2.sdpii.gui.MainFrameController;
 import hogent.sdp2.sdpii.gui.app.AppController;
 import hogent.sdp2.sdpii.gui.app.account.AccountController;
@@ -7,6 +9,7 @@ import hogent.sdp2.sdpii.gui.app.notifications.NotificationsController;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 import javafx.stage.Popup;
@@ -22,6 +25,7 @@ public class HeaderController extends HBox {
     @FXML private Button dropdown_button;
     @FXML private FontIcon button_icon;
     @FXML private HBox trigger_button;
+    @FXML private Label lblUsername;
     private ProfilePopupController pfc;
     private Popup popup;
 
@@ -41,7 +45,12 @@ public class HeaderController extends HBox {
 
         // Trigger op dropdown button
         trigger_button.setOnMouseClicked(e -> togglePopup(trigger_button));
-
+        Werknemer ingelogd = Sessie.getIngelogdeWerknemer();
+        if (ingelogd != null) {
+            lblUsername.setText(ingelogd.getNaam());
+        } else {
+            lblUsername.setText("Gast");
+        }
         Router(app);
     }
 
