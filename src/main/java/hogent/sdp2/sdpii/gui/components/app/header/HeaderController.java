4c52a8027faf615a3bf1fa72f11ed80d346e55c7
CharlesDegraeuwe <charles.degraeuwe@student.hogent.be>
2026-02-23 14:20:09 +0100

    popup toegevoegd

--- src/main/java/hogent/sdp2/sdpii/gui/components/app/header/HeaderController.java
+++ src/main/java/hogent/sdp2/sdpii/gui/components/app/header/HeaderController.java
@@ -8,6 +8,8 @@ import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.control.Button;
 import javafx.scene.layout.HBox;
+import javafx.stage.Popup;
+import org.kordamp.ikonli.javafx.FontIcon;
 
 import java.io.IOException;
 
@@ -16,19 +18,44 @@ public class HeaderController extends HBox {
     @FXML private Button search;
     @FXML private Button notifications;
     @FXML private Button account;
-    @FXML private HBox dropdown;
+    @FXML private Button dropdown_button;
+    @FXML private FontIcon button_icon;
+    private ProfilePopupController pfc;
+    private Popup popup;
 
 
     public HeaderController(AppController app) {
-
         FXMLLoader loader = new FXMLLoader(getClass().getResource("/fmxl/components/app/header/Header.fxml"));
         loader.setRoot(this);
         loader.setController(this);
         try { loader.load(); } catch (IOException e) { throw new RuntimeException(e); }
 
+        pfc = new ProfilePopupController(app);
+
+        // Popup aanmaken
+        popup = new Popup();
+        popup.setAutoHide(true); // sluit als je ergens anders klikt
+        popup.getContent().add(pfc);
+
+        // Trigger op dropdown button
+        dropdown_button.setOnMouseClicked(e -> togglePopup(dropdown_button));
+
         Router(app);
     }
 
+    private void togglePopup(Button trigger) {
+        if (popup.isShowing()) {
+            popup.hide();
+        } else {
+            // Bounds van de button ophalen relatief aan het scherm
+            var bounds = trigger.localToScreen(trigger.getBoundsInLocal());
+            popup.show(
+                    trigger,
+                    bounds.getMinX(),           // x: links uitgelijnd met button
+                    bounds.getMaxY() + 5        // y: net onder de button
+            );
+        }
+    }
     private void Router(AppController app) {
         notifications.setOnMouseClicked(e -> {
             NotificationsController nc = new NotificationsController();
