839b399258407699398acfbae99410725760c0f3
Charles Degraeuwe <charles.degraeuwe@student.hogent.be>
2026-03-09 16:24:14 +0100

    UI updates aan taken

--- src/main/java/hogent/sdp2/sdpii/gui/app/taken/components/manager/assign/AssignTaskController.java
+++ src/main/java/hogent/sdp2/sdpii/gui/app/taken/components/manager/assign/AssignTaskController.java
@@ -1,11 +1,17 @@
 package hogent.sdp2.sdpii.gui.app.taken.components.manager.assign;
 
+import domain.auth.Sessie;
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.control.ComboBox;
 import javafx.scene.layout.BorderPane;
 
 import java.io.IOException;
 
 public class AssignTaskController extends BorderPane {
+    @FXML ComboBox Locatie;
+    @FXML ComboBox Team;
+
     public AssignTaskController() {
         FXMLLoader loader = new FXMLLoader(getClass().getResource("/fmxl/app/taken/manager/assign/AssignTaskLayout.fxml"));
         loader.setRoot(this);
@@ -14,6 +20,19 @@ public class AssignTaskController extends BorderPane {
             loader.load();
         } catch (IOException e) {
             throw new RuntimeException(e);
+        }
+
+        Init();
+    }
+
+    public void Init() {
+        boolean isSupervisor = Sessie.getInstance().isSuperVisor();
+        if(isSupervisor) {
+            Locatie.setVisible(false);
+            Locatie.setManaged(false);
+            Team.setVisible(false);
+            Team.setManaged(false);
+
         }
     }
 }
