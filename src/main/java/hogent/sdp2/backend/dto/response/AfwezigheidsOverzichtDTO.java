80d2c4b06470b310fedd295f9f2948b3b7808fed
Milan Van Bellingen <milan.vanbellingen@student.hogent.be>
2026-03-09 11:33:20 +0100

    planning

---
+++ src/main/java/hogent/sdp2/backend/dto/response/AfwezigheidsOverzichtDTO.java
@@ -0,0 +1,13 @@
+package hogent.sdp2.backend.dto.response;
+
+import java.time.LocalDate;
+
+public record AfwezigheidsOverzichtDTO(
+        Integer werknemerId,
+        String voornaam,
+        String naam,
+        String type,        // "Verlof" of "Ziekte"
+        LocalDate startDatum,
+        LocalDate eindDatum,
+        String status       // voor verlof: Goedgekeurd, In afwachting / voor ziekte: null
+) {}
