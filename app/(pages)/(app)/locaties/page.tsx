7f27d8f4a6deccb51690c40d9eeff0bd8d67ee84
CharlesDegraeuwe <charles.degraeuwe@icloud.com>
2026-04-05 23:58:20 +0200

    feat: added google maps api

--- app/(pages)/(app)/locaties/page.tsx
+++ app/(pages)/(app)/locaties/page.tsx
@@ -1,3 +1,9 @@
+import GoogleMaps from '@/app/(pages)/(app)/locaties/components/googlemaps';
+
 export default function Page() {
-  return <div></div>;
+  return (
+    <div className={'relative z-0 w-full h-full flex'}>
+      <GoogleMaps />
+    </div>
+  );
 }
