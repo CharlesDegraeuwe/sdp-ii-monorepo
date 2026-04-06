4cd896b2cd34be8b061c5e012dd159087718e6b1
CharlesDegraeuwe <charles.degraeuwe@icloud.com>
2026-04-07 01:09:42 +0200

    feat: searchbar

--- app/(pages)/(app)/locaties/page.tsx
+++ app/(pages)/(app)/locaties/page.tsx
@@ -1,8 +1,17 @@
 import GoogleMaps from '@/app/(pages)/(app)/locaties/components/googlemaps';
+import { Container } from '@/components/design system/Container';
+import { Input } from '@/components/design system/Input';
+import { color } from 'listr2';
+import label from '@/components/design system/Label/Label';
 
 export default function Page() {
   return (
     <div className={'relative z-0 w-full h-full flex'}>
+      <div className="flex flex-col gap-5 absolute h-full max-w-1/5 w-1/5 z-40 left-5 top-30">
+        <Container className={'bg-white max-h-3/4 h-3/4 pt-5'}>
+          <Input placeholder={'zoeken...'} color={'white'} />
+        </Container>
+      </div>
       <GoogleMaps />
     </div>
   );
