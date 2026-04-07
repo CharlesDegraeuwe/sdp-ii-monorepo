415b1ee040d5fad3017bc729d194dd859771b7d6
CharlesDegraeuwe <charles.degraeuwe@icloud.com>
2026-04-07 11:55:40 +0200

    feat: overzichtslayout toegevoegd

--- app/(pages)/(app)/overzicht/page.tsx
+++ app/(pages)/(app)/overzicht/page.tsx
@@ -1,3 +1,39 @@
+import { Container } from '@/components/design system/Container';
+
 export default function Page() {
-  return <div></div>;
+  return (
+    <div className="w-full min-h-screen grid grid-cols-4 grid-rows-4 gap-5 px-7 pt-40 py-20">
+      <Container
+        label={'Snelle acties:'}
+        className="col-start-1 col-end-3 row-start-1 row-end-2"
+      />
+
+      <Container
+        label={'Geplande uren:'}
+        className="col-start-1 col-end-3 row-start-2 row-end-5"
+      />
+
+      <Container
+        label={'Kalender:'}
+        className="col-start-3 col-end-4 row-start-1 row-end-3"
+      />
+
+      <Container
+        label={'Notificaties:'}
+        className="col-start-4 col-end-5 row-start-1 row-end-3"
+      />
+
+      <Container
+        label={'Snelle statistieken:'}
+        className="col-start-3 col-end-4 row-start-3 row-end-4"
+      />
+
+      <Container
+        indent={true}
+        className="col-start-4 col-end-5 row-start-3 row-end-4"
+      />
+
+      <Container className="col-start-3 col-end-5 row-start-4 row-end-5" />
+    </div>
+  );
 }
