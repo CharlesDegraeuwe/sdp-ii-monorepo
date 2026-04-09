4a054de2be2c16bd95c7200ea740bb976a6e2c5f
Milan Van Bellingen <milan.vanbellingen@student.hogent.be>
2026-04-09 11:30:15 +0200

    afwezigheid, planner, notif

--- app/(pages)/(app)/afwezigheden/page.tsx
+++ app/(pages)/(app)/afwezigheden/page.tsx
@@ -1,9 +1,3 @@
-import type { Metadata } from 'next';
-
-export const metadata: Metadata = {
-  title: 'Afwezigheden | Delaware Suite',
-};
-
 export default function Page() {
   return <div></div>;
 }
