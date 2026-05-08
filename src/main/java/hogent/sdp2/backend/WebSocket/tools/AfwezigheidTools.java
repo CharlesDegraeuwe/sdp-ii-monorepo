a05357a9bf0284e51985b08fee271c00119dfe57
CharlesDegraeuwe <charles.degraeuwe@icloud.com>
2026-05-08 23:24:29 +0200

    benoit tools

--- src/main/java/hogent/sdp2/backend/WebSocket/tools/AfwezigheidTools.java
+++ src/main/java/hogent/sdp2/backend/WebSocket/tools/AfwezigheidTools.java
@@ -1,15 +1,23 @@
 package hogent.sdp2.backend.WebSocket.tools;
 
+import hogent.sdp2.backend.REST.dto.request.TaakResponseDTO;
+import hogent.sdp2.backend.REST.service.taken.TakenService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.ai.chat.model.ToolContext;
 import org.springframework.ai.tool.annotation.Tool;
 import org.springframework.stereotype.Component;
+
+import java.util.List;
 
 @Component
 @RequiredArgsConstructor
 public class AfwezigheidTools {
 
+    private final TakenService takenService;
     @Tool(description = "Deze tool helpt je met de afwezigheden van een gebruiker op te halen")
-    public void AfwezigheidTools() {
-
+    public void getAbsence(ToolContext ctx) {
+        System.out.println("=== TOOL CALL === getAbsence");
+        System.out.println("=== TOOL CTX === " + ctx.getContext());
+        System.out.println("tool reached");
     }
 }
