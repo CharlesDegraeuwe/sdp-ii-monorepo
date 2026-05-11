34a5e7fd6204f8ecf1771d886fef6e9868d8b071
CharlesDegraeuwe <charles.degraeuwe@icloud.com>
2026-05-11 22:26:33 +0200

    merge

--- app/(pages)/(app)/admin/page.tsx
+++ app/(pages)/(app)/admin/page.tsx
@@ -11,8 +11,8 @@ import {
   IoBusinessOutline,
 } from 'react-icons/io5';
 import Link from '@/components/design-system/Link/Link';
-import BreadcrumbInit from '@/components/app/structuur/breadcrumb/BreadCrumbInit';
 import CreeerGebruikerModal from '@/components/app/admin/creeer-gebruiker/CreeerGebruikerModal';
+import BreadcrumbInit from '@/components/overig/structuur/breadcrumb/BreadCrumbInit';
 
 const links = [
   {
